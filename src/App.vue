5da31e86171b8ac689e18eef1eb62fd8854b3f2d
Zhai Can <bczhc0@126.com>
2024-09-27 11:11:38 +0800

    set up axios and vue server proxy

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
 
 import Demo1 from "./components/Demo1.vue";
+import axios from "axios";
+
+async function click() {
+  let a = await axios.get('/api/routes');
+  console.log(a);
+}
 </script>
 
 <template>
   <h1>hello,</h1>
   <Demo1/>
 
-  <n-button>Button</n-button>
+  <n-button @click="click">Button</n-button>
 </template>
 
 <style scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8081',
+                rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
+    }
 })
